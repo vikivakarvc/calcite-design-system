vikivakarvc/calcite-design-system

commit 74b3b968dd4ec8b0b1240c7e12fe207c26a2a396

fix(date-picker): improve date-picker a11y (#6715)
**Related Issue:** #5570 

## Summary

This improves `calcite-date-picker` a11y by:

* moving `gridcell` role to `calcite-date-picker-month` component
rendering (day wrapper)
* adding `button` role to `calcite-date-picker-day`
* providing a locale-aware `aria-label` to `calcite-date-picker-day`
describing the associated day

src/components/date-picker-day/date-picker-day.scss
@@ -1,10 +1,5 @@
 :host {
-  @apply text-color-3
-    flex
-    min-w-0
-    cursor-pointer
-    justify-center;
-  inline-size: calc(100% / 7);
+  @apply text-color-3 flex cursor-pointer;
 }
 
 @include disabled();

src/components/date-picker-day/date-picker-day.tsx
@@ -11,7 +11,7 @@ import {
 } from "@stencil/core";
 import { dateToISO } from "../../utils/date";
 
-import { closestElementCrossShadowBoundary, getElementDir } from "../../utils/dom";
+import { closestElementCrossShadowBoundary, getElementDir, toAriaBoolean } from "../../utils/dom";
 import { InteractiveComponent, updateHostInteraction } from "../../utils/interactive";
 import { isActivationKey } from "../../utils/key";
 import { numberStringFormatter } from "../../utils/locale";
@@ -40,6 +40,13 @@ export class DatePickerDay implements InteractiveComponent {
 
   /** Day of the month to be shown. */
   @Prop() day!: number;
+
+  /**
+   * The DateTimeFormat used to provide screen reader labels.
+   *
+   * @internal
+   */
+  @Prop() dateTimeFormat: Intl.DateTimeFormat;
 
   /** When `true`, interaction is prevented and the component is displayed with lower opacity. */
   @Prop({ reflect: true }) disabled = false;
@@ -140,8 +147,18 @@ export class DatePickerDay implements InteractiveComponent {
     }
     const formattedDay = numberStringFormatter.localize(String(this.day));
     const dir = getElementDir(this.el);
+    const dayLabel = this.dateTimeFormat.format(this.value);
+
     return (
-      <Host id={dayId} onClick={this.onClick} onKeyDown={this.keyDownHandler} role="gridcell">
+      <Host
+        aria-disabled={toAriaBoolean(this.disabled)}
+        aria-label={dayLabel}
+        aria-selected={toAriaBoolean(this.active)}
+        id={dayId}
+        onClick={this.onClick}
+        onKeyDown={this.keyDownHandler}
+        role="button"
+      >
         <div class={{ "day-v-wrapper": true, [CSS_UTILITY.rtl]: dir === "rtl" }}>
           <div class="day-wrapper">
             <span class="day">

src/components/date-picker-month/date-picker-month.tsx
@@ -36,6 +36,13 @@ export class DatePickerMonth {
   //
   //--------------------------------------------------------------------------
 
+  /**
+   * The DateTimeFormat used to provide screen reader labels.
+   *
+   * @internal
+   */
+  @Prop() dateTimeFormat: Intl.DateTimeFormat;
+
   /** Already selected date.*/
   @Prop() selectedDate: Date;
 
@@ -418,36 +425,39 @@ export class DatePickerMonth {
       (!this.endDate && this.hoverRange && sameDate(this.hoverRange?.end, this.startDate));
 
     return (
-      <calcite-date-picker-day
-        active={active}
-        class={{
-          "hover--inside-range": this.startDate && isHoverInRange,
-          "hover--outside-range": this.startDate && !isHoverInRange,
-          "focused--start": isFocusedOnStart,
-          "focused--end": !isFocusedOnStart
-        }}
-        currentMonth={currentMonth}
-        day={day}
-        disabled={!inRange(date, this.min, this.max)}
-        endOfRange={this.isEndOfRange(date)}
-        highlighted={this.betweenSelectedRange(date)}
-        key={date.toDateString()}
-        onCalciteDaySelect={this.daySelect}
-        onCalciteInternalDayHover={this.dayHover}
-        range={!!this.startDate && !!this.endDate && !sameDate(this.startDate, this.endDate)}
-        rangeHover={this.isRangeHover(date)}
-        scale={this.scale}
-        selected={this.isSelected(date)}
-        startOfRange={this.isStartOfRange(date)}
-        value={date}
-        // eslint-disable-next-line react/jsx-sort-props
-        ref={(el: HTMLCalciteDatePickerDayElement) => {
-          // when moving via keyboard, focus must be updated on active date
-          if (ref && active && this.activeFocus) {
-            el?.focus();
-          }
-        }}
-      />
+      <div class="day" role="gridcell">
+        <calcite-date-picker-day
+          active={active}
+          class={{
+            "hover--inside-range": this.startDate && isHoverInRange,
+            "hover--outside-range": this.startDate && !isHoverInRange,
+            "focused--start": isFocusedOnStart,
+            "focused--end": !isFocusedOnStart
+          }}
+          currentMonth={currentMonth}
+          dateTimeFormat={this.dateTimeFormat}
+          day={day}
+          disabled={!inRange(date, this.min, this.max)}
+          endOfRange={this.isEndOfRange(date)}
+          highlighted={this.betweenSelectedRange(date)}
+          key={date.toDateString()}
+          onCalciteDaySelect={this.daySelect}
+          onCalciteInternalDayHover={this.dayHover}
+          range={!!this.startDate && !!this.endDate && !sameDate(this.startDate, this.endDate)}
+          rangeHover={this.isRangeHover(date)}
+          scale={this.scale}
+          selected={this.isSelected(date)}
+          startOfRange={this.isStartOfRange(date)}
+          value={date}
+          // eslint-disable-next-line react/jsx-sort-props
+          ref={(el: HTMLCalciteDatePickerDayElement) => {
+            // when moving via keyboard, focus must be updated on active date
+            if (ref && active && this.activeFocus) {
+              el?.focus();
+            }
+          }}
+        />
+      </div>
     );
   }
 

src/components/date-picker-day/date-picker-day.e2e.ts
@@ -1,5 +1,7 @@
 import { disabled } from "../../tests/commonTests";
 import { newProgrammaticE2EPage } from "../../tests/utils";
+import { DATE_PICKER_FORMAT_OPTIONS } from "../date-picker/resources";
+import { Serializable } from "puppeteer";
 
 describe("calcite-date-picker-day", () => {
   it("can be disabled", async () => {
@@ -7,11 +9,29 @@ describe("calcite-date-picker-day", () => {
     await page.evaluate(() => {
       const dateEl = document.createElement("calcite-date-picker-day") as HTMLCalciteDatePickerDayElement;
       dateEl.active = true;
+      dateEl.dateTimeFormat = new Intl.DateTimeFormat("en"); // options not needed as this is only needed for rendering
       dateEl.day = 3;
       document.body.append(dateEl);
     });
     await page.waitForChanges();
 
     return disabled({ tag: "calcite-date-picker-day", page });
+  });
+
+  describe("accessibility", () => {
+    it("labels its associated day", async () => {
+      const page = await newProgrammaticE2EPage();
+      await page.evaluate((dateTimeFormatOptions: Intl.DateTimeFormatOptions) => {
+        const dateEl = document.createElement("calcite-date-picker-day") as HTMLCalciteDatePickerDayElement;
+        dateEl.dateTimeFormat = new Intl.DateTimeFormat("en", dateTimeFormatOptions);
+        dateEl.day = 20;
+        dateEl.value = new Date("2020-02-20T08:00:00.000Z");
+        document.body.append(dateEl);
+      }, DATE_PICKER_FORMAT_OPTIONS as Serializable);
+      await page.waitForChanges();
+      const day = await page.find(`calcite-date-picker-day`);
+
+      expect(day.getAttribute("aria-label")).toBe("Thursday, February 20, 2020");
+    });
   });
 });

src/components/date-picker-month/date-picker-month.scss
@@ -19,6 +19,13 @@
   inline-size: calc(100% / 7);
 }
 
+.day {
+  @apply flex
+  min-w-0
+  justify-center;
+  inline-size: calc(100% / 7);
+}
+
 :host([scale="s"]) .week-header {
   @apply text-n2h px-0 pt-2 pb-3;
 }

src/components/date-picker/date-picker.tsx
@@ -29,6 +29,7 @@ import {
 import {
   connectLocalized,
   disconnectLocalized,
+  getDateTimeFormat,
   LocalizedComponent,
   NumberingSystem,
   numberStringFormatter
@@ -42,7 +43,7 @@ import {
 } from "../../utils/t9n";
 import { HeadingLevel } from "../functional/Heading";
 import { DatePickerMessages } from "./assets/date-picker/t9n";
-import { HEADING_LEVEL } from "./resources";
+import { DATE_PICKER_FORMAT_OPTIONS, HEADING_LEVEL } from "./resources";
 import { DateLocaleData, getLocaleData, getValueAsDateRange } from "./utils";
 
 @Component({
@@ -183,20 +184,6 @@ export class DatePicker implements LocalizedComponent, LoadableComponent, T9nCom
    */
   @Event({ cancelable: false }) calciteDatePickerRangeChange: EventEmitter<void>;
 
-  /**
-   * Active start date.
-   */
-  @State() activeStartDate: Date;
-
-  /**
-   * Active end date.
-   */
-  @State() activeEndDate: Date;
-
-  @State() startAsDate: Date;
-
-  @State() endAsDate: Date;
-
   //--------------------------------------------------------------------------
   //
   //  Public Methods
@@ -300,6 +287,25 @@ export class DatePicker implements LocalizedComponent, LoadableComponent, T9nCom
   //
   //--------------------------------------------------------------------------
 
+  /**
+   * Active end date.
+   */
+  @State() activeEndDate: Date;
+
+  /**
+   * Active start date.
+   */
+  @State() activeStartDate: Date;
+
+  /**
+   * The DateTimeFormat used to provide screen reader labels.
+   *
+   * @internal
+   */
+  @State() dateTimeFormat: Intl.DateTimeFormat;
+
+  @State() defaultMessages: DatePickerMessages;
+
   @State() effectiveLocale = "";
 
   @Watch("effectiveLocale")
@@ -307,13 +313,15 @@ export class DatePicker implements LocalizedComponent, LoadableComponent, T9nCom
     updateMessages(this, this.effectiveLocale);
   }
 
-  @State() defaultMessages: DatePickerMessages;
-
-  @State() private localeData: DateLocaleData;
+  @State() endAsDate: Date;
 
   @State() private hoverRange: HoverRange;
 
+  @State() private localeData: DateLocaleData;
+
   private mostRecentRangeValue?: Date;
+
+  @State() startAsDate: Date;
 
   //--------------------------------------------------------------------------
   //
@@ -349,6 +357,7 @@ export class DatePicker implements LocalizedComponent, LoadableComponent, T9nCom
     };
 
     this.localeData = await getLocaleData(this.effectiveLocale);
+    this.dateTimeFormat = getDateTimeFormat(this.effectiveLocale, DATE_PICKER_FORMAT_OPTIONS);
   }
 
   monthHeaderSelectChange = (event: CustomEvent<Date>): void => {
@@ -482,6 +491,7 @@ export class DatePicker implements LocalizedComponent, LoadableComponent, T9nCom
         />,
         <calcite-date-picker-month
           activeDate={activeDate}
+          dateTimeFormat={this.dateTimeFormat}
           endDate={this.range ? endDate : undefined}
           hoverRange={this.hoverRange}
           localeData={this.localeData}

src/components/date-picker/resources.ts
@@ -1,1 +1,3 @@
 export const HEADING_LEVEL = 2;
+
+export const DATE_PICKER_FORMAT_OPTIONS: Intl.DateTimeFormatOptions = { dateStyle: "full" };

commit 073f5ac6b7ffa58bc44648d1bf54115ed83722b8

fix(date-picker): fix range highlight style regression (#6836)
**Related Issue:** N/A

## Summary

This fixes a visual regression introduced by
https://github.com/Esri/calcite-components/pull/6715.

src/components/date-picker-day/date-picker-day.scss
@@ -404,57 +404,57 @@
   }
 }
 
-:host([highlighted]:last-child) :not(.calcite--rtl) .day-wrapper,
-:host([range-hover]:last-child) :not(.calcite--rtl) .day-wrapper,
-:host([highlighted]:first-child) .calcite--rtl .day-wrapper,
-:host([range-hover]:first-child) .calcite--rtl .day-wrapper {
+:host([highlighted][range-edge="end"]) :not(.calcite--rtl) .day-wrapper,
+:host([range-hover][range-edge="end"]) :not(.calcite--rtl) .day-wrapper,
+:host([highlighted][range-edge="start"]) .calcite--rtl .day-wrapper,
+:host([range-hover][range-edge="start"]) .calcite--rtl .day-wrapper {
   box-shadow: inset -4px 0px 0px 0px var(--calcite-ui-foreground-1);
 }
 
-:host([highlighted]:first-child) :not(.calcite--rtl) .day-wrapper,
-:host([range-hover]:first-child) :not(.calcite--rtl) .day-wrapper,
-:host([highlighted]:last-child) .calcite--rtl .day-wrapper,
-:host([range-hover]:last-child) .calcite--rtl .day-wrapper {
+:host([highlighted][range-edge="start"]) :not(.calcite--rtl) .day-wrapper,
+:host([range-hover][range-edge="start"]) :not(.calcite--rtl) .day-wrapper,
+:host([highlighted][range-edge="end"]) .calcite--rtl .day-wrapper,
+:host([range-hover][range-edge="end"]) .calcite--rtl .day-wrapper {
   box-shadow: inset 4px 0px 0px 0px var(--calcite-ui-foreground-1);
 }
 
-:host([scale="s"][highlighted]:last-child) :not(.calcite--rtl) .day-wrapper,
-:host([scale="s"][range-hover]:last-child) :not(.calcite--rtl) .day-wrapper,
-:host([scale="s"][highlighted]:first-child) .calcite--rtl .day-wrapper,
-:host([scale="s"][range-hover]:first-child) .calcite--rtl .day-wrapper {
+:host([scale="s"][highlighted][range-edge="end"]) :not(.calcite--rtl) .day-wrapper,
+:host([scale="s"][range-hover][range-edge="end"]) :not(.calcite--rtl) .day-wrapper,
+:host([scale="s"][highlighted][range-edge="start"]) .calcite--rtl .day-wrapper,
+:host([scale="s"][range-hover][range-edge="start"]) .calcite--rtl .day-wrapper {
   box-shadow: inset -1px 0px 0px 0px var(--calcite-ui-foreground-1);
 }
 
-:host([scale="s"][highlighted]:first-child) :not(.calcite--rtl) .day-wrapper,
-:host([scale="s"][range-hover]:first-child) :not(.calcite--rtl) .day-wrapper,
-:host([scale="s"][highlighted]:last-child) .calcite--rtl .day-wrapper,
-:host([scale="s"][range-hover]:last-child) .calcite--rtl .day-wrapper {
+:host([scale="s"][highlighted][range-edge="start"]) :not(.calcite--rtl) .day-wrapper,
+:host([scale="s"][range-hover][range-edge="start"]) :not(.calcite--rtl) .day-wrapper,
+:host([scale="s"][highlighted][range-edge="end"]) .calcite--rtl .day-wrapper,
+:host([scale="s"][range-hover][range-edge="end"]) .calcite--rtl .day-wrapper {
   box-shadow: inset 1px 0px 0px 0px var(--calcite-ui-foreground-1);
 }
 
-:host([scale="l"][highlighted]:first-child) :not(.calcite--rtl) .day-wrapper,
-:host([scale="l"][range-hover]:first-child) :not(.calcite--rtl) .day-wrapper,
-:host([scale="l"][highlighted]:last-child) .calcite--rtl .day-wrapper,
-:host([scale="l"][range-hover]:last-child) .calcite--rtl .day-wrapper {
+:host([scale="l"][highlighted][range-edge="start"]) :not(.calcite--rtl) .day-wrapper,
+:host([scale="l"][range-hover][range-edge="start"]) :not(.calcite--rtl) .day-wrapper,
+:host([scale="l"][highlighted][range-edge="end"]) .calcite--rtl .day-wrapper,
+:host([scale="l"][range-hover][range-edge="end"]) .calcite--rtl .day-wrapper {
   box-shadow: inset 6px 0px 0px 0px var(--calcite-ui-foreground-1);
 }
 
-:host([highlighted]:first-child) .day-wrapper,
-:host([range-hover]:first-child) .day-wrapper {
+:host([highlighted][range-edge="start"]) .day-wrapper,
+:host([range-hover][range-edge="start"]) .day-wrapper {
   border-start-start-radius: 45%;
   border-end-start-radius: 45%;
 }
 
-:host([highlighted]:last-child) .day-wrapper,
-:host([range-hover]:last-child) .day-wrapper {
+:host([highlighted][range-edge="end"]) .day-wrapper,
+:host([range-hover][range-edge="end"]) .day-wrapper {
   border-start-end-radius: 45%;
   border-end-end-radius: 45%;
 }
 
-:host([scale="l"][highlighted]:last-child) :not(.calcite--rtl) .day-wrapper,
-:host([scale="l"][range-hover]:last-child) :not(.calcite--rtl) .day-wrapper,
-:host([scale="l"][highlighted]:first-child) .calcite--rtl .day-wrapper,
-:host([scale="l"][range-hover]:first-child) .calcite--rtl .day-wrapper {
+:host([scale="l"][highlighted][range-edge="end"]) :not(.calcite--rtl) .day-wrapper,
+:host([scale="l"][range-hover][range-edge="end"]) :not(.calcite--rtl) .day-wrapper,
+:host([scale="l"][highlighted][range-edge="start"]) .calcite--rtl .day-wrapper,
+:host([scale="l"][range-hover][range-edge="start"]) .calcite--rtl .day-wrapper {
   box-shadow: inset -6px 0px 0px 0px var(--calcite-ui-foreground-1);
 }
 

src/components/date-picker-day/date-picker-day.tsx
@@ -63,6 +63,13 @@ export class DatePickerDay implements InteractiveComponent {
   /** When `true`, activates the component's range mode to allow a start and end date. */
   @Prop({ reflect: true }) range = false;
 
+  /**
+   * When `true`, highlight styling for edge dates is applied.
+   *
+   * @internal
+   */
+  @Prop({ reflect: true }) rangeEdge: "start" | "end" | undefined;
+
   /** Date is the start of date range */
   @Prop({ reflect: true }) startOfRange = false;
 

src/components/date-picker-month/date-picker-month.tsx
@@ -16,6 +16,15 @@ import { Scale } from "../interfaces";
 const DAYS_PER_WEEK = 7;
 const DAYS_MAXIMUM_INDEX = 6;
 
+interface Day {
+  active: boolean;
+  currentMonth?: boolean;
+  date: Date;
+  day: number;
+  dayInWeek?: number;
+  ref?: boolean;
+}
+
 @Component({
   tag: "calcite-date-picker-month",
   styleUrl: "date-picker-month.scss",
@@ -194,23 +203,41 @@ export class DatePickerMonth {
     const curMonDays = this.getCurrentMonthDays(month, year);
     const prevMonDays = this.getPreviousMonthDays(month, year, startOfWeek);
     const nextMonDays = this.getNextMonthDays(month, year, startOfWeek);
-    const days = [
+    let dayInWeek = 0;
+    const getDayInWeek = () => dayInWeek++ % 7;
+
+    const days: Day[] = [
       ...prevMonDays.map((day) => {
-        const date = new Date(year, month - 1, day);
-        return this.renderDateDay(false, day, date);
+        return {
+          active: false,
+          day,
+          dayInWeek: getDayInWeek(),
+          date: new Date(year, month - 1, day)
+        };
       }),
       ...curMonDays.map((day) => {
         const date = new Date(year, month, day);
         const active = sameDate(date, this.activeDate);
-        return this.renderDateDay(active, day, date, true, true);
+        return {
+          active,
+          currentMonth: true,
+          day,
+          dayInWeek: getDayInWeek(),
+          date,
+          ref: true
+        };
       }),
       ...nextMonDays.map((day) => {
-        const date = new Date(year, month + 1, day);
-        return this.renderDateDay(false, day, date);
+        return {
+          active: false,
+          day,
+          dayInWeek: getDayInWeek(),
+          date: new Date(year, month + 1, day)
+        };
       })
     ];
 
-    const weeks = [];
+    const weeks: Day[][] = [];
     for (let i = 0; i < days.length; i += 7) {
       weeks.push(days.slice(i, i + 7));
     }
@@ -227,7 +254,7 @@ export class DatePickerMonth {
           </div>
           {weeks.map((days) => (
             <div class="week-days" role="row">
-              {days}
+              {days.map((day) => this.renderDateDay(day))}
             </div>
           ))}
         </div>
@@ -406,26 +433,27 @@ export class DatePickerMonth {
   /**
    * Render calcite-date-picker-day
    *
+   * @param active.active
    * @param active
    * @param day
+   * @param dayInWeek
    * @param date
    * @param currentMonth
    * @param ref
+   * @param active.currentMonth
+   * @param active.date
+   * @param active.day
+   * @param active.dayInWeek
+   * @param active.ref
    */
-  private renderDateDay(
-    active: boolean,
-    day: number,
-    date: Date,
-    currentMonth?: boolean,
-    ref?: boolean
-  ) {
+  private renderDateDay({ active, currentMonth, date, day, dayInWeek, ref }: Day) {
     const isFocusedOnStart = this.isFocusedOnStart();
     const isHoverInRange =
       this.isHoverInRange() ||
       (!this.endDate && this.hoverRange && sameDate(this.hoverRange?.end, this.startDate));
 
     return (
-      <div class="day" role="gridcell">
+      <div class="day" key={date.toDateString()} role="gridcell">
         <calcite-date-picker-day
           active={active}
           class={{
@@ -440,10 +468,10 @@ export class DatePickerMonth {
           disabled={!inRange(date, this.min, this.max)}
           endOfRange={this.isEndOfRange(date)}
           highlighted={this.betweenSelectedRange(date)}
-          key={date.toDateString()}
           onCalciteDaySelect={this.daySelect}
           onCalciteInternalDayHover={this.dayHover}
           range={!!this.startDate && !!this.endDate && !sameDate(this.startDate, this.endDate)}
+          rangeEdge={dayInWeek === 0 ? "start" : dayInWeek === 6 ? "end" : undefined}
           rangeHover={this.isRangeHover(date)}
           scale={this.scale}
           selected={this.isSelected(date)}

src/components/date-picker/date-picker.stories.ts
@@ -123,6 +123,20 @@ export const range = (): string =>
     )}
   </div>`;
 
+export const rangeHighlighted_TestOnly = (): string =>
+  html`
+    <div style="width: 400px">
+      <calcite-date-picker range></calcite-date-picker>
+    </div>
+    <script>
+      (async () => {
+        await customElements.whenDefined("calcite-date-picker");
+        document.querySelector("calcite-date-picker").value = ["2020-02-14", "2020-02-28"];
+        await new Promise((resolve) => requestAnimationFrame(() => resolve()));
+      })();
+    </script>
+  `;
+
 export const rangeRTL_TestOnly = (): string => html`
   <div style="width: 400px">
     <calcite-date-picker value="${testDate}" dir="rtl" range></calcite-date-picker>