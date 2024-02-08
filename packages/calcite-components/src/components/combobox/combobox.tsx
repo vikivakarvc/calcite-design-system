vikivakarvc/calcite-design-system

commit 58317ae6803a3a1716cabeaeac2061fa533a6bad

feat(combobox): limit display of selected items with new selection-display prop (#7912)
**Related Issue:** #4326 

## Summary

This PR introduces a new `selection-display` property to Combobox to
control the chip display with the available values `all`(default), `fit`
and `single`. This new property only takes effect when `selection-mode`
is `multiple` or `ancestors`.

---------

Co-authored-by: Erik Harper <eriklharper@pm.me>

packages/calcite-components/src/components/combobox/combobox.scss
@@ -21,7 +21,7 @@
   --calcite-internal-combobox-input-margin-block: calc(theme("spacing.1") - theme("borderWidth.DEFAULT"));
 
   .x-button {
-    margin-inline-end: theme("spacing.2");
+    margin-inline: theme("spacing.2");
   }
 }
 
@@ -77,25 +77,38 @@
     flex-grow
     flex-wrap
     items-center
+    relative
     truncate
     p-0;
+
+  gap: var(--calcite-combobox-item-spacing-unit-s);
+
+  &.selection-display-fit,
+  &.selection-display-single {
+    @apply flex-nowrap overflow-hidden;
+  }
 }
 
 .input {
-  @apply font-inherit
-  text-color-1
-  flex-grow
-  appearance-none
-  border-none
+  @apply appearance-none
   bg-transparent
+  border-none
+  flex-grow
+  font-inherit
+  text-color-1
+  text-ellipsis
   p-0;
   font-size: inherit;
   block-size: var(--calcite-combobox-input-height);
   line-height: var(--calcite-combobox-input-height);
-  min-inline-size: 120px;
+  inline-size: 100%;
   margin-block-end: var(--calcite-combobox-item-spacing-unit-s);
+  min-inline-size: 4.8125rem;
   &:focus {
     @apply outline-none;
+  }
+  &:placeholder-shown {
+    @apply text-ellipsis;
   }
 }
 
@@ -121,7 +134,12 @@
 
 .input--icon {
   padding-block: 0;
-  padding-inline: var(--calcite-combobox-item-spacing-unit-l);
+  padding-inline: var(--calcite-combobox-item-spacing-unit-s);
+}
+
+:host([scale="m"]) .input--icon,
+:host([scale="l"]) .input--icon {
+  padding-inline: 0.25rem;
 }
 
 .input-wrap {
@@ -195,14 +213,21 @@
   @apply h-0 overflow-hidden;
 }
 
+calcite-chip {
+  --calcite-animation-timing: 0;
+}
+
 .chip {
   margin-block: calc(var(--calcite-combobox-item-spacing-unit-s) / 4);
-  margin-inline: 0 var(--calcite-combobox-item-spacing-unit-s);
   max-inline-size: 100%;
 }
 
 .chip--active {
   @apply bg-foreground-3;
+}
+
+.chip--invisible {
+  @apply absolute invisible;
 }
 
 .item {

packages/calcite-components/src/components/combobox/combobox.tsx
@@ -15,7 +15,12 @@ import {
 import { debounce } from "lodash-es";
 import { filter } from "../../utils/filter";
 
-import { isPrimaryPointerButton, toAriaBoolean } from "../../utils/dom";
+import {
+  getElementWidth,
+  getTextWidth,
+  isPrimaryPointerButton,
+  toAriaBoolean,
+} from "../../utils/dom";
 import {
   connectFloatingUI,
   defaultMenuPlacement,
@@ -46,6 +51,7 @@ import {
 import { connectLabel, disconnectLabel, getLabelText, LabelableComponent } from "../../utils/label";
 import {
   componentFocusable,
+  componentLoaded,
   LoadableComponent,
   setComponentLoaded,
   setUpLoadableComponent,
@@ -62,11 +68,12 @@ import {
 } from "../../utils/t9n";
 import { Scale, SelectionMode } from "../interfaces";
 import { ComboboxMessages } from "./assets/combobox/t9n";
-import { ComboboxChildElement } from "./interfaces";
+import { ComboboxChildElement, SelectionDisplay } from "./interfaces";
 import { ComboboxChildSelector, ComboboxItem, ComboboxItemGroup, CSS } from "./resources";
 import { getItemAncestors, getItemChildren, hasActiveChildren, isSingleLike } from "./utils";
 import { XButton, CSS as XButtonCSS } from "../functional/XButton";
 import { getIconScale } from "../../utils/component";
+import { CoreSizing15 } from "@esri/calcite-design-tokens/dist/es6/calcite-headless";
 
 interface ItemData {
   label: string;
@@ -112,6 +119,12 @@ export class Combobox
    */
   @Prop({ reflect: true }) clearDisabled = false;
 
+  /**
+   * When `selectionMode` is `"ancestors"` or `"multiple"`, specifies the display of multiple `calcite-combobox-item` selections
+   * - `"all"` (displays all selections with individual `calcite-chip`s), `"fit"` (displays individual `calcite-chip`s that scale to the component's size, including a non-closable `calcite-chip`, which provides the number of additional `calcite-combobox-item` selections not visually displayed), or `"single"` (display one `calcite-chip` with the total number of selections).
+   */
+  @Prop({ reflect: true }) selectionDisplay: SelectionDisplay = "all";
+
   /**When `true`, displays and positions the component. */
   @Prop({ reflect: true, mutable: true }) open = false;
 
@@ -142,8 +155,7 @@ export class Combobox
    *
    * When not set, the component will be associated with its ancestor form element, if any.
    */
-  @Prop({ reflect: true })
-  form: string;
+  @Prop({ reflect: true }) form: string;
 
   /** Accessible name for the component. */
   @Prop() label!: string;
@@ -339,7 +351,8 @@ export class Combobox
   /**
    * Updates the position of the component.
    *
-   * @param delayed
+   * @param delayed Reposition the component after a delay
+   * @returns Promise
    */
   @Method()
   async reposition(delayed = false): Promise<void> {
@@ -449,6 +462,10 @@ export class Combobox
     updateHostInteraction(this);
   }
 
+  componentDidUpdate(): void {
+    this.refreshSelectionDisplay();
+  }
+
   disconnectedCallback(): void {
     this.mutationObserver?.disconnect();
     this.resizeObserver?.disconnect();
@@ -465,6 +482,8 @@ export class Combobox
   //  Private State/Props
   //
   //--------------------------------------------------------------------------
+
+  private allSelectedIndicatorChipEl: HTMLCalciteChipElement;
 
   @Element() el: HTMLCalciteComboboxElement;
 
@@ -492,6 +511,12 @@ export class Combobox
 
   @State() activeDescendant = "";
 
+  @State() compactSelectionDisplay = false;
+
+  @State() selectedHiddenChipsCount = 0;
+
+  @State() selectedVisibleChipsCount = 0;
+
   @State() text = "";
 
   /** when search text is cleared, reset active to  */
@@ -515,7 +540,10 @@ export class Combobox
 
   mutationObserver = createObserver("mutation", () => this.updateItems());
 
-  resizeObserver = createObserver("resize", () => this.setMaxScrollerHeight());
+  private resizeObserver = createObserver("resize", () => {
+    this.setMaxScrollerHeight();
+    this.refreshSelectionDisplay();
+  });
 
   private guid = guid();
 
@@ -525,11 +553,17 @@ export class Combobox
 
   private referenceEl: HTMLDivElement;
 
+  private chipContainerEl: HTMLDivElement;
+
   private listContainerEl: HTMLDivElement;
 
   private ignoreSelectedEventsFlag = false;
 
+  private maxCompactBreakpoint: number;
+
   openTransitionProp = "opacity";
+
+  private selectedIndicatorChipEl: HTMLCalciteChipElement;
 
   transitionEl: HTMLDivElement;
 
@@ -782,10 +816,110 @@ export class Combobox
     this.updateActiveItemIndex(targetIndex);
   }
 
+  private hideChip(chipEl: HTMLCalciteChipElement): void {
+    chipEl.classList.add(CSS.chipInvisible);
+  }
+
+  private showChip(chipEl: HTMLCalciteChipElement): void {
+    chipEl.classList.remove(CSS.chipInvisible);
+  }
+
+  private refreshChipDisplay({
+    chipEls,
+    availableHorizontalChipElSpace,
+    chipContainerElGap,
+  }): void {
+    chipEls.forEach((chipEl: HTMLCalciteChipElement) => {
+      if (!chipEl.selected) {
+        this.hideChip(chipEl);
+      } else {
+        const chipElWidth = getElementWidth(chipEl);
+        if (chipElWidth && chipElWidth < availableHorizontalChipElSpace) {
+          availableHorizontalChipElSpace -= chipElWidth + chipContainerElGap;
+          this.showChip(chipEl);
+          return;
+        }
+      }
+      this.hideChip(chipEl);
+    });
+  }
+
+  private refreshSelectionDisplay = async () => {
+    await componentLoaded(this);
+
+    if (isSingleLike(this.selectionMode)) {
+      return;
+    }
+
+    if (!this.textInput) {
+      return;
+    }
+
+    const {
+      allSelectedIndicatorChipEl,
+      chipContainerEl,
+      selectionDisplay,
+      placeholder,
+      selectedIndicatorChipEl,
+      textInput,
+    } = this;
+
+    const chipContainerElGap = parseInt(getComputedStyle(chipContainerEl).gap.replace("px", ""));
+    const chipContainerElWidth = getElementWidth(chipContainerEl);
+    const { fontSize, fontFamily } = getComputedStyle(textInput);
+    const inputTextWidth = getTextWidth(placeholder, `${fontSize} ${fontFamily}`);
+    const inputWidth = (inputTextWidth || parseInt(CoreSizing15)) + chipContainerElGap;
+    const allSelectedIndicatorChipElWidth = getElementWidth(allSelectedIndicatorChipEl);
+    const selectedIndicatorChipElWidth = getElementWidth(selectedIndicatorChipEl);
+    const largestSelectedIndicatorChipWidth = Math.max(
+      allSelectedIndicatorChipElWidth,
+      selectedIndicatorChipElWidth
+    );
+
+    this.setCompactSelectionDisplay({
+      chipContainerElGap,
+      chipContainerElWidth,
+      inputWidth,
+      largestSelectedIndicatorChipWidth,
+    });
+
+    if (selectionDisplay === "fit") {
+      const chipEls = Array.from(this.el.shadowRoot.querySelectorAll("calcite-chip")).filter(
+        (chipEl) => chipEl.closable
+      );
+
+      let availableHorizontalChipElSpace = Math.round(
+        chipContainerElWidth -
+          ((this.selectedHiddenChipsCount > 0 ? selectedIndicatorChipElWidth : 0) +
+            chipContainerElGap +
+            inputWidth +
+            chipContainerElGap)
+      );
+
+      this.refreshChipDisplay({ availableHorizontalChipElSpace, chipContainerElGap, chipEls });
+      this.setVisibleAndHiddenChips(chipEls);
+    }
+  };
+
   setFloatingEl = (el: HTMLDivElement): void => {
     this.floatingEl = el;
     connectFloatingUI(this, this.referenceEl, this.floatingEl);
   };
+
+  private setCompactSelectionDisplay({
+    chipContainerElGap,
+    chipContainerElWidth,
+    inputWidth,
+    largestSelectedIndicatorChipWidth,
+  }): void {
+    const newCompactBreakpoint = Math.round(
+      largestSelectedIndicatorChipWidth + chipContainerElGap + inputWidth
+    );
+    if (!this.maxCompactBreakpoint || this.maxCompactBreakpoint < newCompactBreakpoint) {
+      this.maxCompactBreakpoint = newCompactBreakpoint;
+    }
+    this.compactSelectionDisplay = chipContainerElWidth < this.maxCompactBreakpoint;
+  }
 
   setContainerEl = (el: HTMLDivElement): void => {
     this.resizeObserver.observe(el);
@@ -793,10 +927,40 @@ export class Combobox
     this.transitionEl = el;
   };
 
+  setChipContainerEl = (el: HTMLDivElement): void => {
+    this.resizeObserver.observe(el);
+    this.chipContainerEl = el;
+  };
+
   setReferenceEl = (el: HTMLDivElement): void => {
     this.referenceEl = el;
     connectFloatingUI(this, this.referenceEl, this.floatingEl);
   };
+
+  setAllSelectedIndicatorChipEl = (el: HTMLCalciteChipElement): void => {
+    this.allSelectedIndicatorChipEl = el;
+  };
+
+  setSelectedIndicatorChipEl = (el: HTMLCalciteChipElement): void => {
+    this.selectedIndicatorChipEl = el;
+  };
+
+  private setVisibleAndHiddenChips(chipEls: HTMLCalciteChipElement[]): void {
+    let newSelectedVisibleChipsCount = 0;
+    chipEls.forEach((chipEl) => {
+      if (chipEl.selected && !chipEl.classList.contains(CSS.chipInvisible)) {
+        newSelectedVisibleChipsCount++;
+      }
+    });
+    if (newSelectedVisibleChipsCount !== this.selectedVisibleChipsCount) {
+      this.selectedVisibleChipsCount = newSelectedVisibleChipsCount;
+    }
+    const newSelectedHiddenChipsCount =
+      this.getSelectedItems().length - newSelectedVisibleChipsCount;
+    if (newSelectedHiddenChipsCount !== this.selectedHiddenChipsCount) {
+      this.selectedHiddenChipsCount = newSelectedHiddenChipsCount;
+    }
+  }
 
   private getMaxScrollerHeight(): number {
     const items = this.getItemsAndGroups().filter((item) => !item.hidden);
@@ -942,7 +1106,7 @@ export class Combobox
     return this.items.filter((item) => !item.hidden);
   }
 
-  getSelectedItems(): HTMLCalciteComboboxItemElement[] {
+  private getSelectedItems = (): HTMLCalciteComboboxItemElement[] => {
     if (!this.isMulti()) {
       const match = this.items.find(({ selected }) => selected);
       return match ? [match] : [];
@@ -964,7 +1128,7 @@ export class Combobox
           return bIdx - aIdx;
         })
     );
-  }
+  };
 
   private updateItems = (): void => {
     this.items = this.getItems();
@@ -1135,6 +1299,10 @@ export class Combobox
     }
   }
 
+  private isAllSelected(): boolean {
+    return this.getItems().length === this.getSelectedItems().length;
+  }
+
   isMulti(): boolean {
     return !isSingleLike(this.selectionMode);
   }
@@ -1174,6 +1342,7 @@ export class Combobox
           messageOverrides={{ dismissLabel: messages.removeTag }}
           onCalciteChipClose={() => this.calciteChipCloseHandler(item)}
           scale={scale}
+          selected={item.selected}
           title={label}
           value={item.value}
         >
@@ -1181,6 +1350,149 @@ export class Combobox
         </calcite-chip>
       );
     });
+  }
+
+  renderAllSelectedIndicatorChip(): VNode {
+    const {
+      compactSelectionDisplay,
+      scale,
+      selectedVisibleChipsCount,
+      setAllSelectedIndicatorChipEl,
+    } = this;
+    const label = this.messages.allSelected;
+    return (
+      <calcite-chip
+        class={{
+          chip: true,
+          [CSS.chipInvisible]: !(
+            this.isAllSelected() &&
+            !selectedVisibleChipsCount &&
+            !compactSelectionDisplay
+          ),
+        }}
+        scale={scale}
+        title={label}
+        value=""
+        ref={setAllSelectedIndicatorChipEl}
+      >
+        {label}
+      </calcite-chip>
+    );
+  }
+
+  renderAllSelectedIndicatorChipCompact(): VNode {
+    const { compactSelectionDisplay, scale, selectedVisibleChipsCount } = this;
+    const label = this.messages.all || "All";
+    return (
+      <calcite-chip
+        class={{
+          chip: true,
+          [CSS.chipInvisible]: !(
+            this.isAllSelected() &&
+            !selectedVisibleChipsCount &&
+            compactSelectionDisplay
+          ),
+        }}
+        scale={scale}
+        title={label}
+        value=""
+      >
+        {label}
+      </calcite-chip>
+    );
+  }
+
+  renderSelectedIndicatorChip(): VNode {
+    const {
+      compactSelectionDisplay,
+      selectionDisplay,
+      getSelectedItems,
+      scale,
+      selectedHiddenChipsCount,
+      selectedVisibleChipsCount,
+      setSelectedIndicatorChipEl,
+    } = this;
+    let chipInvisible, label;
+    if (compactSelectionDisplay) {
+      chipInvisible = true;
+    } else {
+      if (selectionDisplay === "single") {
+        const selectedItemsCount = getSelectedItems().length;
+        if (this.isAllSelected()) {
+          chipInvisible = true;
+        } else if (selectedItemsCount > 0) {
+          chipInvisible = false;
+        } else {
+          chipInvisible = true;
+        }
+        label = `${selectedItemsCount} ${this.messages.selected}`;
+      } else if (selectionDisplay === "fit") {
+        if (
+          (this.isAllSelected() && selectedVisibleChipsCount === 0) ||
+          selectedHiddenChipsCount === 0
+        ) {
+          chipInvisible = true;
+        } else {
+          chipInvisible = false;
+        }
+        label =
+          selectedVisibleChipsCount > 0
+            ? `+${selectedHiddenChipsCount}`
+            : `${selectedHiddenChipsCount} ${this.messages.selected}`;
+      }
+    }
+    return (
+      <calcite-chip
+        class={{
+          chip: true,
+          [CSS.chipInvisible]: chipInvisible,
+        }}
+        ref={setSelectedIndicatorChipEl}
+        scale={scale}
+        title={label}
+        value=""
+      >
+        {label}
+      </calcite-chip>
+    );
+  }
+
+  renderSelectedIndicatorChipCompact(): VNode {
+    const {
+      compactSelectionDisplay,
+      selectionDisplay,
+      getSelectedItems,
+      scale,
+      selectedHiddenChipsCount,
+    } = this;
+    let chipInvisible, label;
+    if (compactSelectionDisplay) {
+      const selectedItemsCount = getSelectedItems().length;
+      if (this.isAllSelected()) {
+        chipInvisible = true;
+      } else if (selectionDisplay === "fit") {
+        chipInvisible = selectedHiddenChipsCount > 0 ? false : true;
+        label = `${selectedHiddenChipsCount || 0}`;
+      } else if (selectionDisplay === "single") {
+        chipInvisible = selectedItemsCount > 0 ? false : true;
+        label = `${selectedItemsCount}`;
+      }
+    } else {
+      chipInvisible = true;
+    }
+    return (
+      <calcite-chip
+        class={{
+          chip: true,
+          [CSS.chipInvisible]: chipInvisible,
+        }}
+        scale={scale}
+        title={label}
+        value=""
+      >
+        {label}
+      </calcite-chip>
+    );
   }
 
   renderInput(): VNode {
@@ -1315,10 +1627,12 @@ export class Combobox
   }
 
   render(): VNode {
-    const { guid, label, open } = this;
-    const single = isSingleLike(this.selectionMode);
+    const { selectionDisplay, guid, label, open } = this;
+    const singleSelectionMode = isSingleLike(this.selectionMode);
+    const allSelectionDisplay = selectionDisplay === "all";
+    const singleSelectionDisplay = selectionDisplay === "single";
+    const fitSelectionDisplay = !singleSelectionMode && selectionDisplay === "fit";
     const isClearable = !this.clearDisabled && this.value?.length > 0;
-
     return (
       <Host onClick={this.comboboxFocusHandler}>
         <div
@@ -1331,7 +1645,7 @@ export class Combobox
           aria-owns={`${listboxUidPrefix}${guid}`}
           class={{
             wrapper: true,
-            "wrapper--single": single || !this.selectedItems.length,
+            "wrapper--single": singleSelectionMode || !this.selectedItems.length,
             "wrapper--active": open,
           }}
           onClick={this.clickHandler}
@@ -1340,9 +1654,23 @@ export class Combobox
           // eslint-disable-next-line react/jsx-sort-props -- ref should be last so node attrs/props are in sync (see https://github.com/Esri/calcite-design-system/pull/6530)
           ref={this.setReferenceEl}
         >
-          <div class="grid-input">
+          <div
+            class={{
+              "grid-input": true,
+              [CSS.selectionDisplayFit]: fitSelectionDisplay,
+              [CSS.selectionDisplaySingle]: singleSelectionDisplay,
+            }}
+            ref={this.setChipContainerEl}
+          >
             {this.renderIconStart()}
-            {!single && this.renderChips()}
+            {!singleSelectionMode && !singleSelectionDisplay && this.renderChips()}
+            {!singleSelectionMode &&
+              !allSelectionDisplay && [
+                this.renderSelectedIndicatorChip(),
+                this.renderSelectedIndicatorChipCompact(),
+                this.renderAllSelectedIndicatorChip(),
+                this.renderAllSelectedIndicatorChipCompact(),
+              ]}
             <label
               class="screen-readers-only"
               htmlFor={`${inputUidPrefix}${guid}`}

commit 0eb8b5d12b287ee1c51c38500e6041e6fa3acc5c

fix(combobox): avoid inline-start padding on combobox label when icon is displayed (#8672)
**Related Issue:** #8396 

## Summary

This fixes a spacing regression introduced in
https://github.com/Esri/calcite-design-system/pull/7912/.

packages/calcite-components/src/components/combobox/combobox.scss
@@ -142,12 +142,7 @@
 
 .input--icon {
   padding-block: 0;
-  padding-inline: var(--calcite-combobox-item-spacing-unit-s);
-}
-
-:host([scale="m"]) .input--icon,
-:host([scale="l"]) .input--icon {
-  padding-inline: 0.25rem;
+  padding-inline: var(--calcite-combobox-item-spacing-unit-l);
 }
 
 .input-wrap {

packages/calcite-components/src/components/combobox/combobox.tsx
@@ -1520,7 +1520,18 @@ export class Combobox
     );
   }
 
-  renderInput(): VNode {
+  private get showingInlineIcon(): boolean {
+    const { placeholderIcon, selectionMode, selectedItems, open } = this;
+    const selectedItem = selectedItems[0];
+    const selectedIcon = selectedItem?.icon;
+    const singleSelectionMode = isSingleLike(selectionMode);
+
+    return !open && selectedItem
+      ? !!selectedIcon && singleSelectionMode
+      : !!placeholderIcon && (!selectedItem || singleSelectionMode);
+  }
+
+  private renderInput(): VNode {
     const { guid, disabled, placeholder, selectionMode, selectedItems, open } = this;
     const single = isSingleLike(selectionMode);
     const selectedItem = selectedItems[0];
@@ -1554,7 +1565,7 @@ export class Combobox
             "input--single": true,
             "input--transparent": this.activeChipIndex > -1,
             "input--hidden": showLabel,
-            "input--icon": !!this.placeholderIcon,
+            "input--icon": this.showingInlineIcon && !!this.placeholderIcon,
           }}
           disabled={disabled}
           id={`${inputUidPrefix}${guid}`}
@@ -1614,19 +1625,13 @@ export class Combobox
     );
   }
 
-  renderIconStart(): VNode {
-    const { selectedItems, placeholderIcon, selectionMode, placeholderIconFlipRtl } = this;
+  renderSelectedOrPlaceholderIcon(): VNode {
+    const { selectedItems, placeholderIcon, placeholderIconFlipRtl } = this;
     const selectedItem = selectedItems[0];
     const selectedIcon = selectedItem?.icon;
-    const singleSelectionMode = isSingleLike(selectionMode);
-
-    const iconAtStart =
-      !this.open && selectedItem
-        ? !!selectedIcon && singleSelectionMode
-        : !!this.placeholderIcon && (!selectedItem || singleSelectionMode);
 
     return (
-      iconAtStart && (
+      this.showingInlineIcon && (
         <span class="icon-start">
           <calcite-icon
             class="selected-icon"
@@ -1639,7 +1644,7 @@ export class Combobox
     );
   }
 
-  renderIconEnd(): VNode {
+  renderChevronIcon(): VNode {
     const { open } = this;
     return (
       <span class="icon-end">
@@ -1680,6 +1685,7 @@ export class Combobox
             // eslint-disable-next-line react/jsx-sort-props -- ref should be last so node attrs/props are in sync (see https://github.com/Esri/calcite-design-system/pull/6530)
             ref={this.setReferenceEl}
           >
+            {this.renderSelectedOrPlaceholderIcon()}
             <div
               class={{
                 "grid-input": true,
@@ -1688,7 +1694,6 @@ export class Combobox
               }}
               ref={this.setChipContainerEl}
             >
-              {this.renderIconStart()}
               {!singleSelectionMode && !singleSelectionDisplay && this.renderChips()}
               {!singleSelectionMode &&
                 !allSelectionDisplay && [
@@ -1714,7 +1719,7 @@ export class Combobox
                 scale={this.scale}
               />
             ) : null}
-            {this.renderIconEnd()}
+            {this.renderChevronIcon()}
           </div>
           <ul
             aria-labelledby={`${labelUidPrefix}${guid}`}